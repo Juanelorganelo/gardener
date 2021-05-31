1241de6883d38e42ad827d34fecd70a05f024116
Juan Manuel <cokenrocknroll@gmail.com>
2021-05-31 16:46:51 -0500

    docs: add TypeDoc comments

--- test/support/write-config.ts
+++ test/support/write-config.ts
@@ -1,3 +1,8 @@
+/**
+ * A fancy-test plugin to write a Gardener configuration fixture.
+ * @author Juan Manuel Garcia Junco Moreno
+ * @copyright CourseKey Inc. All rights reserved
+ */
 import fs from "fs";
 import path from "path";
 import yaml from "yaml";
@@ -5,10 +10,22 @@ import yaml from "yaml";
 import type { FancyTypes } from "@oclif/test";
 import type { Config } from "../../src/config";
 
+/**
+ * Context used by the write-config plugin.
+ */
 interface WriteConfigContext extends FancyTypes.Context {
+  /**
+   * The file path to the configuration.
+   */
   file: string;
 }
 
+/**
+ * A fancy-test plugin to write Gardener's configuration fixtures.
+ * @param config The Gardener configuration to write.
+ * @param file The path to the configuration file. Defaults to '.gardenerrc.yml' on PWD.
+ * @return The fancy-test plugin.
+ */
 export function writeConfig(
   config: Partial<Config>,
   file = path.join(process.cwd(), ".gardenerrc.yml"),
