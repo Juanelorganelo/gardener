2476ddebb9445d7dc1dfbd96e8bc693e54e06542
Juan Manuel <cokenrocknroll@gmail.com>
2021-05-31 18:05:04 -0500

    style: format tests

--- test/config.test.ts
+++ test/config.test.ts
@@ -1,29 +1,31 @@
-import {expect} from '@oclif/test'
-import {test} from './support'
-import Gardener from '../src'
+import { expect } from "@oclif/test";
+import { test } from "./support";
+import Gardener from "../src";
 
-describe('config', () => {
+describe("config", () => {
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['foo']))
-  .catch(error => {
-    expect(error.message).to.contain('Invalid branch name. GitFlow branches must match the following pattern <type>/<description>')
-  })
-  .it('uses a git-flow preset by default')
-
-  test
-  .stdout()
-  .do(() => Gardener.run(['foo', '--exclude', 'foo']))
-  .it('skips the given branch from linting', ctx => {
-    expect(ctx.stdout).to.contain('Branch foo configured as excluded, skipping')
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain(
+        "Invalid branch name. GitFlow branches must match the following pattern <type>/<description>",
+      );
+    })
+    .it("uses a git-flow preset by default");
 
   test
-  .stdout()
-  .config({exclude: ['foo']})
-  .do(() => Gardener.run(['foo']))
-  .it('uses the config defined in the rc file', ctx => {
-    expect(ctx.stdout).to.contain('Branch foo configured as excluded, skipping')
-  })
-})
+    .stdout()
+    .do(() => Gardener.run(["foo", "--exclude", "foo"]))
+    .it("skips the given branch from linting", (ctx) => {
+      expect(ctx.stdout).to.contain("Branch foo configured as excluded, skipping");
+    });
+
+  test
+    .stdout()
+    .config({ exclude: ["foo"] })
+    .do(() => Gardener.run(["foo"]))
+    .it("uses the config defined in the rc file", (ctx) => {
+      expect(ctx.stdout).to.contain("Branch foo configured as excluded, skipping");
+    });
+});
--- test/git-flow.test.ts
+++ test/git-flow.test.ts
@@ -1,88 +1,88 @@
-import {expect} from '@oclif/test'
-import Gardener from '../src'
-import {test} from './support'
+import { expect } from "@oclif/test";
+import Gardener from "../src";
+import { test } from "./support";
 
-describe('git-flow', () => {
+describe("git-flow", () => {
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['feature/foo']))
-  .it('allows feature branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
-
-  test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['feature/foo']))
-  .it('allows bugfix branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["feature/foo"]))
+    .it("allows feature branches", (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['feature/foo']))
-  .it('allows hotfix branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["feature/foo"]))
+    .it("allows bugfix branches", (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['feature/foo']))
-  .it('allows release branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["feature/foo"]))
+    .it("allows hotfix branches", (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['main']))
-  .it('doesnt error on "main" branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["feature/foo"]))
+    .it("allows release branches", (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['master']))
-  .it('doesnt error on "master" branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["main"]))
+    .it('doesnt error on "main" branches', (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['develop']))
-  .it('doesnt error on "develop" branches', ctx => {
-    expect(ctx.stderr).to.be.empty
-  })
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["master"]))
+    .it('doesnt error on "master" branches', (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
 
   test
-  .stdout()
-  .stderr()
-  .config({
-    preset: {
-      name: 'git-flow',
-      options: {
-        issueNumber: {
-          pattern: 'FF-\\d+',
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["develop"]))
+    .it('doesnt error on "develop" branches', (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .config({
+      preset: {
+        name: "git-flow",
+        options: {
+          issueNumber: {
+            pattern: "FF-\\d+",
+          },
         },
       },
-    },
-  })
-  .do(() => Gardener.run(['feature/foo']))
-  .catch(error => {
-    expect(error.message).to.contain('Invalid branch description. Missing issue number')
-  })
-  .it('should enforce issue number patterns')
+    })
+    .do(() => Gardener.run(["feature/foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid branch description. Missing issue number");
+    })
+    .it("should enforce issue number patterns");
 
   test
-  .stdout()
-  .stderr()
-  .do(() => Gardener.run(['feature/foo-FF-3453']))
-  .it('should enforce issue number patterns', ctx => {
-    expect(ctx.stdout).to.be.empty
-  })
-})
+    .stdout()
+    .stderr()
+    .do(() => Gardener.run(["feature/foo-FF-3453"]))
+    .it("should enforce issue number patterns", (ctx) => {
+      expect(ctx.stdout).to.be.empty;
+    });
+});
