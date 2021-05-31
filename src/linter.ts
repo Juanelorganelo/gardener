6e48c73d7310785ecb75125251b31e4e9a511e0a
Juan Manuel <cokenrocknroll@gmail.com>
2021-05-31 17:24:16 -0500

    style: format linter code

--- src/linter.ts
+++ src/linter.ts
@@ -1,41 +1,41 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import {Presets} from './presets/preset'
-import {GitFlowPreset} from './presets/git-flow'
-import {PatternPreset} from './presets/pattern'
+import { Presets } from "./presets/preset";
+import { GitFlowPreset } from "./presets/git-flow";
+import { PatternPreset } from "./presets/pattern";
 
-import type {Preset} from './presets/preset'
+import type { Preset } from "./presets/preset";
 
-import type {Config} from './config'
+import type { Config } from "./config";
 
 export async function lint(branchName: string, config: Config): Promise<string | undefined> {
-  const preset = createPreset(config)
-  const validation = await preset.validate(branchName)
+  const preset = createPreset(config);
+  const validation = await preset.validate(branchName);
 
   if (validation) {
-    return validation
+    return validation;
   }
 }
 
 function createPreset(config: Config): Preset {
-  let name: string
-  let options: Record<string, unknown> | undefined
+  let name: string;
+  let options: Record<string, unknown> | undefined;
 
-  if (typeof config.preset === 'string') {
-    name = config.preset
+  if (typeof config.preset === "string") {
+    name = config.preset;
   } else {
-    name = config.preset.name
-    options = config.preset.options
+    name = config.preset.name;
+    options = config.preset.options;
   }
 
   // Clean up the name.
-  name = name.trim()
+  name = name.trim();
 
   switch (name) {
-  case Presets.PATTERN:
-    return new PatternPreset(options)
-  case Presets.GIT_FLOW:
-    return new GitFlowPreset(options)
-  default:
-    throw new Error(`Invalid preset name ${name}`)
+    case Presets.PATTERN:
+      return new PatternPreset(options);
+    case Presets.GIT_FLOW:
+      return new GitFlowPreset(options);
+    default:
+      throw new Error(`Invalid preset name ${name}`);
   }
 }
