4e2e0de791c62785fa552ba8ff0a9216daddf0b8
Juan Manuel <cokenrocknroll@gmail.com>
2021-05-31 18:44:53 -0500

    tests: add pattern preset tests

---
+++ test/pattern.test.ts
@@ -0,0 +1,44 @@
+/**
+ * Tests for the pattern preset.
+ * @author Juan Manuel Garcia Junco Moreno
+ * @copyright CourseKey Inc. All rights reserved
+ */
+import { expect } from "@oclif/test";
+import Gardener from "../src";
+import { test } from "./support";
+
+describe("pattern", () => {
+  test
+    .stdout()
+    .config({
+      preset: {
+        name: "pattern",
+        options: {
+          pattern: "^[\\w-]$",
+        },
+      },
+    })
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain(
+        "Invalid branch name foo. Branch names must match regular expression ^[\\w-]$",
+      );
+    })
+    .it("should ensure branch matches a regular expression");
+
+  test
+    .stdout()
+    .stderr()
+    .config({
+      preset: {
+        name: "pattern",
+        options: {
+          pattern: "^[\\w-]+$",
+        },
+      },
+    })
+    .do(() => Gardener.run(["foo-bar"]))
+    .it("passes on branches that match the regular expression", (ctx) => {
+      expect(ctx.stderr).to.be.empty;
+    });
+});
