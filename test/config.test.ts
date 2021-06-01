998a7f2d20ba13c10472058f0b2c7049880fe073
Juan Manuel <cokenrocknroll@gmail.com>
2021-06-01 12:32:33 -0500

    tests: add config test cases

--- test/config.test.ts
+++ test/config.test.ts
@@ -28,4 +28,135 @@ describe("config", () => {
     .it("uses the config defined in the rc file", (ctx) => {
       expect(ctx.stdout).to.contain("Branch foo configured as excluded, skipping");
     });
+
+  test
+    .stdout()
+    .config({ preset: "foo" })
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid preset");
+    })
+    .it("should throw on invalid preset names");
+
+  test
+    .stdout()
+    .config({ exclude: ["foo"] })
+    .do(() => Gardener.run(["bar", "--exclude", "bar"]))
+    .it("overrides the exclude option with the --exclude flag", (ctx) => {
+      expect(ctx.stdout).to.contain("Branch bar configured as excluded, skipping");
+    });
+
+  test
+    .stdout()
+    .config({
+      preset: {
+        name: "pattern",
+        options: {
+          pattern: "^\\w+$",
+        },
+      },
+    })
+    .do(() => Gardener.run(["feature/foo", "--preset", "git-flow"]))
+    .it("overrides the preset option with the --preset flag", (ctx) => {
+      expect(ctx.stdout).to.be.empty;
+    });
+
+  test
+    .stdout()
+    .config(
+      {
+        exclude: ["foo"],
+      },
+      { format: "js" },
+    )
+    .do(() => Gardener.run(["foo"]))
+    .it("supports config files written in JavaScript", (ctx) => {
+      expect(ctx.stdout).to.contain("Branch foo configured as excluded, skipping");
+    });
+
+  test
+    .stdout()
+    .config(
+      {
+        exclude: ["foo"],
+      },
+      { format: "json" },
+    )
+    .do(() => Gardener.run(["foo"]))
+    .it("supports config files written in JSON", (ctx) => {
+      expect(ctx.stdout).to.contain("Branch foo configured as excluded, skipping");
+    });
+
+  test
+    .stdout()
+    .config({
+      foo: "bar",
+      exclude: ["foo"],
+    } as any)
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("should throw on configuration files with extra properties");
+
+  test
+    .stdout()
+    .config({
+      exclude: "foo",
+    } as any)
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("throws if exclude property is not an array");
+
+  test
+    .stdout()
+    .config({
+      exclude: ["foo", true],
+    } as any)
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("throws if exclude property has non-string values");
+
+  test
+    .stdout()
+    .config({
+      preset: {
+        name: "git-flow",
+      },
+    })
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("throws if preset is an object without an options property");
+
+  test
+    .stdout()
+    .config({
+      preset: {
+        options: {},
+      } as any,
+    })
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("throws if preset is an object without an name property");
+
+  test
+    .stdout()
+    .config({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      preset: 3,
+    })
+    .do(() => Gardener.run(["foo"]))
+    .catch((error) => {
+      expect(error.message).to.contain("Invalid configuration file");
+    })
+    .it("throws if preset is not an object nor a string");
 });
--- test/support/write-config.ts
+++ test/support/write-config.ts
@@ -6,9 +6,15 @@
 import fs from "fs";
 import path from "path";
 import yaml from "yaml";
+import serializeJavaScript from "serialize-javascript";
 
 import type { FancyTypes } from "@oclif/test";
 import type { Config } from "../../src/config";
+
+/**
+ * Available configuration formats.
+ */
+type ConfigFormat = "yaml" | "json" | "js";
 
 /**
  * Context used by the write-config plugin.
@@ -21,6 +27,22 @@ interface WriteConfigContext extends FancyTypes.Context {
 }
 
 /**
+ * Options for the write-config plugin.
+ */
+interface WriteConfigOptions {
+  /**
+   * The path that the config will be created in.
+   * Default depends on the configuration format.
+   */
+  file?: string;
+  /**
+   * The format that the config file will be written in.
+   * Defaults to "yaml".
+   */
+  format?: ConfigFormat;
+}
+
+/**
  * A fancy-test plugin to write Gardener's configuration fixtures.
  * @param config The Gardener configuration to write.
  * @param file The path to the configuration file. Defaults to '.gardenerrc.yml' on PWD.
@@ -28,16 +50,40 @@ interface WriteConfigContext extends FancyTypes.Context {
  */
 export function writeConfig(
   config: Partial<Config>,
-  file = path.join(process.cwd(), ".gardenerrc.yml"),
+  { file, format }: WriteConfigOptions = {},
 ): FancyTypes.Plugin<WriteConfigContext> {
+  let content: string;
+  switch (format) {
+    case "js":
+      content = `module.exports = ${serializeJavaScript(config)}`;
+      break;
+    case "yaml":
+      content = yaml.stringify(config);
+      break;
+    default:
+      content = JSON.stringify(config);
+      break;
+  }
+
+  const f = file ?? resolveConfig(format);
+
   return {
     run(ctx: WriteConfigContext) {
-      ctx.file = file;
-      fs.writeFileSync(file, yaml.stringify(config));
+      ctx.file = f;
+      fs.writeFileSync(f, content);
     },
     finally(ctx: WriteConfigContext) {
       fs.unlinkSync(ctx.file);
       ctx.file = "";
     },
   };
+}
+
+/**
+ * Get the path to a configuration file.
+ * @param format The format of the configuration file.
+ * @return The path to the configuration file.
+ */
+function resolveConfig(format?: ConfigFormat): string {
+  return path.resolve(process.cwd(), format ? `.gardenerrc.${format}` : ".gardenerrc");
 }
